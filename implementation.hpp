2858a60b32736b105cafbc694b9504ab4ccc44bc
lucas <lucasatracz@outlook.com>
2020-04-23 18:06:18 -0300

    Added try, move some functions
    Added basic try checking
    Code reformatation
    Move setup_imgui_context and setup_imgui_impl to private functions.

--- implementation.hpp
+++ implementation.hpp
@@ -23,6 +23,7 @@
 
 // DirectX includes
 #include <d3d9.h>
+#include <exception>
 #include <string> //std::string
 #pragma comment(lib, "d3d9.lib")
 
@@ -54,53 +55,53 @@ public:
     };
 protected:
     // If a m_hwnd is not nullptr then CreateWindowA has returned success and we have a window.
-    HWND m_hwnd{};
+    HWND        m_hwnd          {};
     // If m_wnd_class is not nullptr, then our window structure provided by user parameters has been generated.
-    WNDCLASSEX m_window_class{};
+    WNDCLASSEX  m_window_class  {};
 
     // Builder will provide the necessary arguments for creating our window class and then the window.
     // If CreateWindowA fails then m_hwnd will be nullptr.
-    Implementation_Window(const std::string& window_name,
-        int width, int heigth,
-        const style_class& class_style,
-        const std::string& class_name,
-        int initial_pos_x, int initial_pos_y) :
-        m_window_class{ sizeof(WNDCLASSEX),
-                        static_cast<unsigned int>(class_style),
-                        WndProc,
-                        0,
-                        0,
-                        GetModuleHandleA(nullptr),
-                        nullptr, nullptr, nullptr, nullptr,
-                        class_name.c_str(),
-                        nullptr }
+    Implementation_Window( const std::string& window_name,
+                           const int width, const int heigth,
+                           const style_class& class_style,
+                           const std::string& class_name,
+                           const int initial_pos_x, const int initial_pos_y ) : 
+           m_window_class{ sizeof( WNDCLASSEX ),
+                           static_cast< unsigned int >( class_style ),
+                           WndProc,
+                           0,
+                           0,
+                           GetModuleHandleA( nullptr ),
+                           nullptr, nullptr, nullptr, nullptr,
+                           class_name.c_str(),
+                           nullptr }
     {
-        RegisterClassExA(&m_window_class);
-        m_hwnd = CreateWindowExA(0LU,
-                        m_window_class.lpszClassName,
-                        window_name.c_str(),
-                        WS_OVERLAPPEDWINDOW,
-                        initial_pos_x, initial_pos_y,
-                        width, heigth,
-                        nullptr, nullptr,
-                        m_window_class.hInstance,
-                        nullptr);
+        RegisterClassExA        ( &m_window_class );
+        m_hwnd = CreateWindowExA( 0LU,
+                                  m_window_class.lpszClassName,
+                                  window_name.c_str(),
+                                  WS_OVERLAPPEDWINDOW,
+                                  initial_pos_x, initial_pos_y,
+                                  width, heigth,
+                                  nullptr, nullptr,
+                                  m_window_class.hInstance,
+                                  nullptr );
     }
 
     // The destructor will destroy our window, invalidating m_hwnd and unregistering the window.
     ~Implementation_Window()
     {
-        DestroyWindow(m_hwnd);
-        UnregisterClassA(m_window_class.lpszClassName, m_window_class.hInstance);
+        DestroyWindow   ( m_hwnd                                                 );
+        UnregisterClassA( m_window_class.lpszClassName, m_window_class.hInstance );
     }
 public:
     // No builders available to create a new object of type
-    Implementation_Window(const Implementation_Window&) = delete;
-    Implementation_Window& operator=(const Implementation_Window&) = delete;
+    Implementation_Window( const Implementation_Window& )               = delete;
+    Implementation_Window& operator=( const Implementation_Window& )    = delete;
 
     // Const references return to m_hwnd and m_wnd_class that can only be accessed.
-    const HWND& get_hwnd() const { return m_hwnd; }
-    const WNDCLASSEX& get_wnd() const { return m_window_class; }
+    const HWND&         get_hwnd()  const { return m_hwnd;          }
+    const WNDCLASSEX&   get_wnd()   const { return m_window_class;  }
 };
 
 // It has device controland directx functions.
@@ -108,14 +109,14 @@ class Implementation_Render : public Implementation_Window
 {
 protected:
     // Release the buffer via "Release()" and set the pointer to nullptr, so it is ready to use again.
-    void clear_device_d3d()
+    void clear_device_d3d() noexcept
     {
-        if (g_pd3d_device)
+        if ( g_pd3d_device )
         {
             g_pd3d_device->Release();
             g_pd3d_device = nullptr;
         }
-        if (g_pd3d)
+        if ( g_pd3d )
         {
             g_pd3d->Release();
             g_pd3d = nullptr;
@@ -123,49 +124,61 @@ protected:
     }
 
     // Creates the device and defines vsync or not.
-    bool create_device_d3d(const bool vsync = true)
+    bool create_device_d3d( const bool vsync = true )
     {
-        if ((g_pd3d = Direct3DCreate9(D3D_SDK_VERSION)) == NULL)
+        if ( ( g_pd3d = Direct3DCreate9( D3D_SDK_VERSION ) ) == NULL )
             return false;
 
         // Create the D3DDevice
-        ZeroMemory(&g_d3dpp, sizeof(g_d3dpp));
+        ZeroMemory( &g_d3dpp, sizeof( g_d3dpp ) );
         g_d3dpp.Windowed = TRUE;
         g_d3dpp.SwapEffect = D3DSWAPEFFECT_DISCARD;
         g_d3dpp.BackBufferFormat = D3DFMT_UNKNOWN;
         g_d3dpp.EnableAutoDepthStencil = TRUE;
         g_d3dpp.AutoDepthStencilFormat = D3DFMT_D16;
-        // Present with vsync
-        // Present without vsync, maximum unthrottled framerate
-        // The boolean vsync define this.
-        g_d3dpp.PresentationInterval = (vsync ? D3DPRESENT_INTERVAL_ONE : D3DPRESENT_INTERVAL_IMMEDIATE);
-        if (g_pd3d->CreateDevice(D3DADAPTER_DEFAULT, D3DDEVTYPE_HAL, m_hwnd, D3DCREATE_HARDWARE_VERTEXPROCESSING, &g_d3dpp, &g_pd3d_device) < 0)
+        // Present with vertical sincronization
+        // Present without vertical sincronization, maximum unthrottled framerate
+        // The boolean m_vertical_sincronization define this.
+        g_d3dpp.PresentationInterval = ( vsync ? D3DPRESENT_INTERVAL_ONE : D3DPRESENT_INTERVAL_IMMEDIATE );
+        if ( g_pd3d->CreateDevice( D3DADAPTER_DEFAULT, D3DDEVTYPE_HAL, m_hwnd, D3DCREATE_HARDWARE_VERTEXPROCESSING, &g_d3dpp, &g_pd3d_device ) < 0 )
             return false;
 
         return true;
     }
 
     // The builder is responsible for calling the "Implementation_Window" builder that will create a window.
-    Implementation_Render(const std::string& window_name,
-        int width, int heigth,
-        const style_class& class_style,
-        const std::string& class_name,
-        int initial_pos_x, int initial_pos_y) :
-        Implementation_Window(window_name,
-                         width, heigth,
-                         class_style,
-                         class_name,
-                         initial_pos_x, initial_pos_y)
+    Implementation_Render( const std::string& window_name,
+                           const int width, const int heigth,
+                           const style_class& class_style,
+                           const std::string& class_name,
+                           const int initial_pos_x, const int initial_pos_y ) :
+    Implementation_Window( window_name,
+                           width, heigth,
+                           class_style,
+                           class_name,
+                           initial_pos_x, initial_pos_y)
     {
-        // Then the device is created and the window updated.
-        // If create_window_d3d fails then the device is cleared.
-        if (!create_device_d3d())
+        try
         {
-            clear_device_d3d();
+            // Then the device is created and the window updated.
+            // If create_window_d3d fails then the device is cleared.
+            if ( !create_device_d3d() )
+            {
+                clear_device_d3d();
+                throw std::exception("Create device D3D failed!");
+            }
         }
-
-        ShowWindow(m_hwnd, SW_SHOWDEFAULT);
-        UpdateWindow(m_hwnd);
+        catch ( const std::exception &e )
+        {
+            MessageBoxA(m_hwnd, e.what(), "Exception", MB_ABORTRETRYIGNORE | MB_ICONSTOP);
+            #if defined _DEBUG
+                __debugbreak();
+            #else
+                exit(0);
+            #endif
+        }
+        ShowWindow  ( m_hwnd, SW_SHOWDEFAULT );
+        UpdateWindow( m_hwnd                 );
     }
 
     // Clear device
@@ -175,20 +188,20 @@ protected:
     }
 public:
     //No builders available to create a new object of type
-    Implementation_Render(const Implementation_Render&) = delete;
-    Implementation_Render& operator=(const Implementation_Render&) = delete;
+    Implementation_Render( const Implementation_Render& )               = delete;
+    Implementation_Render& operator=( const Implementation_Render& )    = delete;
 
     // The reset device is a static function because WindowProc uses it
     static void reset_device();
 };
 
 //const std::string& window_name
-//int width = 1280
-//int heigth = 800
+//const int width = 1280
+//const int heigth = 800
 //const style_class& class_style = style_class::CLASSDC
 //const std::string& class_name = "ImGui Basic Setup"
-//int initial_x = 10
-//int initial_y = 10
+//const int initial_x = 10
+//const int initial_y = 10
 class Implementation final : public Implementation_Render
 {
 public:
@@ -202,22 +215,49 @@ public:
 private:
     // MSG struct to while loop main.cpp
     MSG m_msg{};
+
+    // Initializes the context of imgui and sets the color style to classic.
+    void setup_imgui_context()
+    {
+        ImGui::CreateContext();
+        set_style( style_color::CLASSIC );
+    }
+
+    // Implements Win32 and DirectX
+    // get_hwnd() returns a reference to the m_hwnd variable
+    void setup_imgui_impl()
+    {
+        if( !ImGui_ImplWin32_Init( get_hwnd() ) || !ImGui_ImplDX9_Init( g_pd3d_device ) )
+            throw std::exception( "Implementation Win32 or DX9 ImGui failed!" );
+    }
 public:
     //window_name is required.
-    Implementation(const std::string& window_name,
-        int width = 1280, int heigth = 800,
-        const style_class& class_style = style_class::CLASSDC,
-        const std::string& class_name = "ImGui Basic Setup",
-        int initial_pos_x = 10, int initial_pos_y = 10) :
-        Implementation_Render(window_name,
-                         width, heigth,
-                         class_style,
-                         class_name,
-                         initial_pos_x, initial_pos_y)
+    Implementation( const std::string& window_name,
+                    const int width = 1280, const int heigth = 800,
+                    const style_class& class_style = style_class::CLASSDC,
+                    const std::string& class_name = "ImGui Basic Setup",
+                    const int initial_pos_x = 10, const int initial_pos_y = 10 ) :
+    Implementation_Render( window_name,
+                           width, heigth,
+                           class_style,
+                           class_name,
+                           initial_pos_x, initial_pos_y )
     {
-        // Initializes the context of imgui, then implements Win32 and DirectX and sets the color style to classic.
-        setup_imgui_context();
-
+        try
+        {
+            // Initializes the context of imgui and sets the color style to classic.
+            setup_imgui_context();
+            setup_imgui_impl();
+        }
+        catch ( const std::exception& e )
+        {
+            MessageBoxA( m_hwnd, e.what(), "Exception", MB_ABORTRETRYIGNORE | MB_ICONSTOP );
+            #if defined _DEBUG
+                __debugbreak();
+            #else
+                exit(0);
+            #endif
+        }
         memset(&m_msg, 0, sizeof(m_msg));
     }
 
@@ -230,9 +270,9 @@ public:
     }
 
     // Toggles color style simply by making a call and passing a member of the enum style_color
-    void set_style(const style_color& style) const
+    void set_style( const style_color& style ) const
     {
-        switch (style)
+        switch ( style )
         {
         case style_color::CLASSIC:
             ImGui::StyleColorsClassic();
@@ -251,26 +291,13 @@ public:
 
     // A wrapper for the background color of the window.
     // If no arguments are passed it will be black.
-    void set_background_color(const int r = 0, const int g = 0, const int b = 0)
+    void set_background_color( const int r = 0, const int g = 0, const int b = 0 )
     {
-        g_pd3d_device->SetRenderState(D3DRS_ZENABLE, FALSE);
-        g_pd3d_device->SetRenderState(D3DRS_ALPHABLENDENABLE, FALSE);
-        g_pd3d_device->SetRenderState(D3DRS_SCISSORTESTENABLE, FALSE);
-        D3DCOLOR clear_col_dx = D3DCOLOR_RGBA(r, g, b, 255);
-        g_pd3d_device->Clear(0, NULL, D3DCLEAR_TARGET | D3DCLEAR_ZBUFFER, clear_col_dx, 1.0f, 0);
-    }
-
-    // Initializes the context of imgui, then implements Win32 and DirectX and sets the color style to classic.
-    // get_hwnd() returns a reference to the m_hwnd variable
-    void setup_imgui_context()
-    {
-        ImGui::CreateContext();
-        ImGuiIO& io = ImGui::GetIO(); (void)io;
-
-        ImGui::StyleColorsClassic();
-
-        assert(ImGui_ImplWin32_Init(get_hwnd()) && "ImGui Context: failed ImplWin32!");
-        assert(ImGui_ImplDX9_Init(g_pd3d_device) && "ImGui Context: failed ImplDX9!");
+        g_pd3d_device->SetRenderState( D3DRS_ZENABLE, 0             );
+        g_pd3d_device->SetRenderState( D3DRS_ALPHABLENDENABLE, 0    );
+        g_pd3d_device->SetRenderState( D3DRS_SCISSORTESTENABLE, 0   );
+        D3DCOLOR clear_col_dx = D3DCOLOR_RGBA( r, g, b, 255 );
+        g_pd3d_device->Clear( 0, nullptr, D3DCLEAR_TARGET | D3DCLEAR_ZBUFFER, clear_col_dx, 1.0f, 0 );
     }
 
     // A wrapper for the New Frame
@@ -286,15 +313,15 @@ public:
     void end_frame()
     {
         ImGui::EndFrame();
-        if (g_pd3d_device->BeginScene() >= 0)
+        if ( g_pd3d_device->BeginScene() >= 0 )
         {
             ImGui::Render();
-            ImGui_ImplDX9_RenderDrawData(ImGui::GetDrawData());
+            ImGui_ImplDX9_RenderDrawData( ImGui::GetDrawData() );
             g_pd3d_device->EndScene();
         }
-        HRESULT result = g_pd3d_device->Present(NULL, NULL, NULL, NULL);
+        HRESULT result = g_pd3d_device->Present( nullptr, nullptr, nullptr, nullptr);
 
-        if (result == D3DERR_DEVICELOST && g_pd3d_device->TestCooperativeLevel() == D3DERR_DEVICENOTRESET)
+        if ( result == D3DERR_DEVICELOST && g_pd3d_device->TestCooperativeLevel() == D3DERR_DEVICENOTRESET )
             reset_device();
     }
 
@@ -307,34 +334,34 @@ public:
 void Implementation_Render::reset_device()
 {
     ImGui_ImplDX9_InvalidateDeviceObjects();
-    HRESULT hr = g_pd3d_device->Reset(&g_d3dpp);
-    if (hr == D3DERR_INVALIDCALL)
+    HRESULT hr = g_pd3d_device->Reset( &g_d3dpp );
+    if ( hr == D3DERR_INVALIDCALL )
         IM_ASSERT(0);
     ImGui_ImplDX9_CreateDeviceObjects();
 }
 
-LRESULT __stdcall WndProc(HWND hWnd, UINT msg, WPARAM wParam, LPARAM lParam)
+LRESULT __stdcall WndProc( HWND hWnd, UINT msg, WPARAM wParam, LPARAM lParam )
 {
-    if (ImGui_ImplWin32_WndProcHandler(hWnd, msg, wParam, lParam))
+    if ( ImGui_ImplWin32_WndProcHandler( hWnd, msg, wParam, lParam ) )
         return true;
 
-    switch (msg)
+    switch ( msg )
     {
     case WM_SIZE:
-        if (g_pd3d_device != NULL && wParam != SIZE_MINIMIZED)
+        if ( g_pd3d_device != NULL && wParam != SIZE_MINIMIZED )
         {
-            g_d3dpp.BackBufferWidth = LOWORD(lParam);
-            g_d3dpp.BackBufferHeight = HIWORD(lParam);
+            g_d3dpp.BackBufferWidth =   LOWORD ( lParam );
+            g_d3dpp.BackBufferHeight =  HIWORD ( lParam );
             Implementation_Render::reset_device();
         }
         return 0;
     case WM_SYSCOMMAND:
-        if ((wParam & 0xfff0) == SC_KEYMENU) // Disable ALT application menu
+        if ( ( wParam & 0xfff0 ) == SC_KEYMENU ) // Disable ALT application menu
             return 0;
         break;
     case WM_DESTROY:
-        ::PostQuitMessage(0);
+        PostQuitMessage( 0 );
         return 0;
     }
-    return DefWindowProcA(hWnd, msg, wParam, lParam);
+    return DefWindowProcA( hWnd, msg, wParam, lParam );
 }
--- main.cpp
+++ main.cpp
@@ -4,15 +4,22 @@ int main()
 {
     // Initialize new object of type 'Implement'
     // Window name is required
-    Implementation imgui { "ImGui DirectX9 Implementation", 800, 600 };
+    Implementation imgui{ "ImGui DirectX9 Implementation", 800, 600 };
+
+    //Example variables
+    bool show_demo_window       { true };
+    bool show_another_window    { true };
+    float float_slider          { 0.0f };
+    int counter                 { 0    };
+    ImVec4 simple_color         { 0.40f, 0.50f, 0.60f, 1.0f };
 
     // Loop message
-    while (imgui.get_msg().message != WM_QUIT)
+    while ( imgui.get_msg().message != WM_QUIT )
     {
-        if (PeekMessageA(&imgui.get_msg(), nullptr, 0U, 0U, PM_REMOVE))
+        if ( PeekMessageA( &imgui.get_msg(), nullptr, 0U, 0U, PM_REMOVE ) )
         {
-            TranslateMessage(&imgui.get_msg());
-            DispatchMessageA(&imgui.get_msg());
+            TranslateMessage( &imgui.get_msg() );
+            DispatchMessageA( &imgui.get_msg() );
             continue;
         }
         
@@ -20,17 +27,37 @@ int main()
         imgui.start_frame();
 
         // Your code start here
+        if ( show_demo_window )
+            ImGui::ShowDemoWindow( &show_demo_window );
 
-        if (ImGui::Begin("Some title"))
         {
-            ImGui::Text("Hello World!");
-            if (ImGui::Button("Light Style"))
-            {
-                imgui.set_style(Implementation::style_color::LIGHT);
-            }
-        } 
-        ImGui::End();
+            ImGui::Begin( "Hello, world!" );                              // Create a window called "Hello, world!" and append into it.
 
+            ImGui::Text( "This is some useful text." );                   // Display some text (you can use a format strings too)
+            ImGui::Checkbox( "Demo Window", &show_demo_window );          // Edit bools storing our window open/close state
+            ImGui::Checkbox( "Another Window", &show_another_window );
+
+            ImGui::SliderFloat( "float", &float_slider, 0.0f, 1.0f );     // Edit 1 float using a slider from 0.0f to 1.0f
+            ImGui::ColorEdit3( "simple color", reinterpret_cast< float* >( &simple_color ) );   // Edit 3 floats representing a color
+            if ( ImGui::Button( "Dark Style Color" ) )
+                imgui.set_style( Implementation::style_color::DARK );
+            if ( ImGui::Button( "Button" ) )                                // Buttons return true when clicked (most widgets return true when edited/activated)
+                counter++;
+            ImGui::SameLine();
+            ImGui::Text( "counter = %d", counter );
+
+            ImGui::Text( "Application average %.3f ms/frame (%.1f FPS)", 1000.0f / ImGui::GetIO().Framerate, ImGui::GetIO().Framerate );
+            ImGui::End();
+        }
+
+        if ( show_another_window )
+        {
+            ImGui::Begin( "Another Window", &show_another_window );       // Pass a pointer to our bool variable (the window will have a closing button that will clear the bool when clicked)
+            ImGui::Text( "Hello from another window!" );
+            if ( ImGui::Button( "Close Me" ) )
+                show_another_window = false;
+            ImGui::End();
+        }
         // Your code end here
 
         // End frame
